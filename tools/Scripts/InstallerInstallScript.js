8cc20f694c9edbff728d878ed3fa375bcb1c43b9
Andrew Sazonov <AndrewSazonov@users.noreply.github.com>
2022-07-13 14:50:56 +0200

    Fix .desktop file name

--- tools/Scripts/InstallerInstallScript.js
+++ tools/Scripts/InstallerInstallScript.js
@@ -97,8 +97,8 @@ Component.prototype.createOperations = function()
 
     component.addOperation(
       "Copy",
-      "@TargetDir@/easyDiffraction.desktop",
-      "@HomeDir@/.local/share/applications/easyDiffraction.desktop"
+      "@TargetDir@/@ProductName@.desktop",
+      "@HomeDir@/.local/share/applications/@ProductName@.desktop"
     )
 
     /*

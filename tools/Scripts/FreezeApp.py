8093f04d833d588604f69ab32ac21c61a38fd8ab
Piotr Rozyczko <piotr.rozyczko@gmail.com>
2023-02-27 15:26:18 +0100

    add debug console to the installer

--- tools/Scripts/FreezeApp.py
+++ tools/Scripts/FreezeApp.py
@@ -133,10 +133,10 @@ def runPyInstaller():
         pyInstallerMain([
             main_py_path,                           # Application main file
             f'--name={CONFIG.app_name}',            # Name to assign to the bundled app and spec file (default: first script’s basename)
-            '--log-level', 'WARN',                  # LEVEL may be one of DEBUG, INFO, WARN, ERROR, CRITICAL (default: INFO).
+            '--log-level', 'DEBUG',                  # LEVEL may be one of DEBUG, INFO, WARN, ERROR, CRITICAL (default: INFO).
             '--noconfirm',                          # Replace output directory (default: SPECPATH/dist/SPECNAME) without asking for confirmation
             '--clean',                              # Clean PyInstaller cache and remove temporary files before building
-            '--windowed',                           # Windows and Mac OS X: do not provide a console window for standard i/o.
+            # '--windowed',                           # Windows and Mac OS X: do not provide a console window for standard i/o.
             '--onedir',                             # Create a one-folder bundle containing an executable (default)
             #'--specpath', workDirPath(),           # Folder to store the generated spec file (default: current directory)
             '--distpath', CONFIG.dist_dir,          # Where to put the bundled app (default: ./dist)
